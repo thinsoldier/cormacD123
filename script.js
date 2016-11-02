1a54efd3d166341cf6bb2ea70185459eb468cc77
thinsoldier <thinsoldier@thinsoldier.com>
2016-11-02 09:10:52 -0600

    Refactor js.
    Avoid nested hell. Individual named functions are better for debugging complex behaviors.

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
+$(document).ready( setupObservers );
 
-  $(document).ready(function(){ 
-            $("#getJSON").click(function(){
-                var url = "json_data.php";
-                url = url + "?reference=" + $('#reference').val();
-                
-                $.getJSON(url,function(json){
-                    $("#msg").empty();
-                    $("#msg").append("getJSON");
-                    $("#firstname").val(json.members[0].firstname);
-                    $("#lastname").val(json.members[0].lastname);
-                    $("#carmake").val(json.members[0].carmake);
-                    $("#carmodel").val(json.members[0].carmodel);
-                    $("#year").val(json.members[0].year);
-                    $("#miles").val(json.members[0].miles);
-                    $("#registration").val(json.members[0].registration);
-                    $("#annualprice").val(json.members[0].annualprice);
-                    $("#monthlyprice").val(json.members[0].monthlyprice);
-                
-                    
-                });
-            });
-         });
+function setupObservers()
+{
+	$("#getJSON").click(function(){
+		$.getJSON( "json_data.php" + "?reference=" + $('#reference').val(), updateForm );
+	});
+}
+
+function updateForm( json )
+{
+	$("#msg").empty();
+	$("#msg").append("getJSON");
+	$("#firstname").val(json.members[0].firstname);
+	$("#lastname").val(json.members[0].lastname);
+	$("#carmake").val(json.members[0].carmake);
+	$("#carmodel").val(json.members[0].carmodel);
+	$("#year").val(json.members[0].year);
+	$("#miles").val(json.members[0].miles);
+	$("#registration").val(json.members[0].registration);
+	$("#annualprice").val(json.members[0].annualprice);
+	$("#monthlyprice").val(json.members[0].monthlyprice);                    
+}
