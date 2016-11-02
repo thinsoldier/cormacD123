b3ff7d5d1104d3eb0c7f76c0518b08adaf236e31
thinsoldier <thinsoldier@thinsoldier.com>
2016-11-02 08:58:41 -0600

    Refactor php.
    If you don't care if the request is $_GET or $_POST just use $_REQUEST
    
    Instead of a bunch of if statements around 1 variable you can use a switch statement or use each expected/allowed value as the key of an array.
    
    Generic php objects are converted to generic javascript object via json_encode so you don't have to manually write any of the JSON syntax.

--- json_data.php
+++ json_data.php
@@ -1,13 +1,40 @@
 <?php
-(isset($_POST["reference"]))?
-    $parameter_ref = $_POST["reference"]:
-    $parameter_ref = $_GET["reference"];
-    
-    if ($parameter_ref === "CGD123")
-        $arr[0] = array('firstname' => "John", 'lastname' => "Smith", 'carmake' => "Ford", 'carmodel' => "Focus", 'year' => "2007", 'miles' => "112000", 'registration' => "ABC1234", 'annualprice' => "658.49", 'monthlyprice' => "64.00");
-    else if ($parameter_ref === "DVD123")
-            $arr[0] = array('firstname' => "Joey", 'lastname' => "Diaz", 'carmake' => "Audi", 'carmodel' => "A3", 'year' => "2010", 'miles' => "80000", 'registration' => "JDZ4321", 'annualprice' => "838.99", 'monthlyprice' => "75.00");
 
-echo '{"members":'.json_encode($arr).'}';
+// new generic object
+$output = new stdClass();
 
+$output->members = array();
+
+$records = array(
+	"CGD123" => array(
+		'firstname' => "John", 
+		'lastname' => "Smith", 
+		'carmake' => "Ford", 
+		'carmodel' => "Focus", 
+		'year' => "2007", 
+		'miles' => "112000", 
+		'registration' => "ABC1234", 
+		'annualprice' => "658.49", 
+		'monthlyprice' => "64.00"
+	),
+	"DVD123" => array(
+		'firstname' => "Joey", 
+		'lastname' => "Diaz", 
+		'carmake' => "Audi", 
+		'carmodel' => "A3", 
+		'year' => "2010", 
+		'miles' => "80000", 
+		'registration' => "JDZ4321",
+		'annualprice' => "838.99", 
+		'monthlyprice' => "75.00"
+	)
+);
+
+// Search records list for a matching key
+if( isset( $_REQUEST["reference"] ) && isset( $records[ $_REQUEST["reference"] ] ) )
+{
+	$output->members[0] = $records[ $_REQUEST["reference"] ];
+}
+
+echo json_encode($output);
 ?>
